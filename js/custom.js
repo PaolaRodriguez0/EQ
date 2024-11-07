e992fffa75c7462f57ee0ddacbb4b7a23c2739f8
PaolaRodriguez0 <paolarodriguez@rmasb.com>
2024-11-07 09:50:45 -0500

    Js comit

---
+++ js/custom.js
@@ -0,0 +1,112 @@
+/**	
+	* SinglePro HTML 1.0	
+	* Template Scripts
+	* Created by WpFreeware Team
+
+	Custom JS
+	
+	1. Superslides Slider
+	2. Fixed Top Menubar
+	3. Featured Slider
+	4. Skill Circle
+	5. Wow animation
+	6. Project Counter
+	7. TEAM SLIDER
+	8. BLOG SLIDER
+	9. TESTIMONIAL SLIDER
+	10. CLIENTS SLIDER
+	11. Google Map
+	12. SCROLL TOP BUTTON
+	13. PRELOADER 
+	14. MENU SCROLL 
+	15. MOBILE MENU CLOSE 	
+	
+**/
+
+jQuery(function($){
+
+
+	/* ----------------------------------------------------------- */
+	/*  1. Superslides Slider
+	/* ----------------------------------------------------------- */
+	jQuery('#slides').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	jQuery('#slides2').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+    jQuery('#slides3').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides4').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides5').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides6').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides7').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides8').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides9').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides10').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides11').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides12').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides13').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides14').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	jQuery('#slides15').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+	 jQuery('#slides16').superslides({
+      animation: 'slide',
+      play: false
+    });
+	
+});
